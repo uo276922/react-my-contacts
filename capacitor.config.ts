428f9d97af7acf0226535571f60a7f091573cd55
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2024-04-25 19:56:45 +0200

    Terminado guión hasta parte obligatoria, sin la parte de Android Studio

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'react-my-contacts',
+  webDir: 'build'
+};
+
+export default config;
