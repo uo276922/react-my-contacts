0f8c47fd56b32082aa08ff4e91c4a94af2d6c184
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2024-04-25 19:45:59 +0200

    Terminado guión hasta el despliegue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import CardList from "./components/CardList";
+import Scroll from "./components/Scroll";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./components/ErrorFallback";
+import Searcher from "./components/Searcher";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=50")
+      .then((response) => response.json())
+      .then((data) => setContacts(data.results));
+  }, []);
+
+  const [searchField, setSearchField] = useState("");
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  };
+  const searchedContacts = contacts.filter((contact) => {
+    return (contact.name["first"] + " " + contact.name["last"])
+      .toLowerCase()
+      .includes(searchField.toLowerCase());
+  });
+
+  const onAZ = () => {
+    let az = contacts.sort((a, b) => {
+      return (a.name["first"] + " " + a.name["last"]).localeCompare(
+        b.name["first"] + " " + b.name["last"]
+      );
+    });
+    setContacts([...az]); //clone the list
+  };
+  const onZA = () => {
+    let za = contacts.sort((a, b) => {
+      return (b.name["first"] + " " + b.name["last"]).localeCompare(
+        a.name["first"] + " " + a.name["last"]
+      );
+    });
+    setContacts([...za]); //clone the list
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="tc ">
+      <header>
+        <h1 className="f1">My contacts</h1>
       </header>
+      {contacts.length === 0 ? (
+        <h2 className="f2">Loading...</h2>
+      ) : (
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Searcher searchChange={onSearchChange} az={onAZ} za={onZA} />
+          <Scroll>
+            <CardList contacts={searchedContacts} />
+          </Scroll>
+        </ErrorBoundary>
+      )}
+      <footer>
+        <hr />
+        <p>
+          Desarrollo de Software para Dispositivos Moviles.
+          {new Date().getFullYear()}
+        </p>
+      </footer>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Scroll.js
@@ -0,0 +1,6 @@
+const Scroll = ({ children }) => {
+  return (
+    <div className="vh-75 ba bw2 br4 b--blue overflow-y-scroll">{children}</div>
+  );
+};
+export default Scroll;
