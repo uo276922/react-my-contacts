605a12333a02c42b5114b6a20f6a050250488ee9
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2024-04-28 11:09:09 +0200

    Correccion en el filtro por genero en el despliegue 2

--- src/components/Searcher.js
+++ src/components/Searcher.js
@@ -3,6 +3,19 @@ import { BiSortAZ, BiSortZA } from "react-icons/bi";
 const Searcher = ({ searchChange, az, za, onGenderChange }) => {
   return (
     <div className="pa2 bg-lightest-blue flex justify-center items-center">
+      <select onChange={onGenderChange} className="pa2  mb2 ma3">
+        <option value="">All Genders</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+      </select>
+
+      <input
+        className="ba b--blue bg-light-green pa3 br3 w-20"
+        type="search"
+        placeholder="search contacts"
+        onChange={searchChange}
+      />
       <div className="mr3  f1 ml3">
         <a href="#0" title="Ascending order" onClick={az}>
           <BiSortAZ />
@@ -11,21 +24,6 @@ const Searcher = ({ searchChange, az, za, onGenderChange }) => {
           <BiSortZA />
         </a>
       </div>
-      <input
-        className="ba b--blue bg-light-green pa3 br3 w-20"
-        type="search"
-        placeholder="search contacts"
-        onChange={searchChange}
-      />
-      <select
-        onChange={onGenderChange}
-        className="pa2 mb2 ml3"
-      >
-        <option value="">All Genders</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="other">Other</option>
-      </select>
     </div>
   );
 };
